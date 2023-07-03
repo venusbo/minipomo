5e6a1a19f99d1e8bb184654474585d6ddc98c820
venusbo <109348740+venusbo@users.noreply.github.com>
2023-07-03 17:09:00 +1000

    fixed break function to 'reset' after completion

--- script-pomodoro.js
+++ script-pomodoro.js
@@ -1,5 +1,6 @@
 var pomodoro;
 var ele = document.getElementById('pomodoro')
+var ele1 = document.getElementById('statusPomodoro')
 
 
 function startBreak(){
@@ -9,7 +10,13 @@ function startBreak(){
         sec = sec-1;
         min = Math.floor(sec/60);
         ele.innerHTML = min + ":" + sec%60
-    }, 1000)}
+        ele1.innerHTML = "break time ^_^"
+
+        if(sec <= 0){
+    
+            reset();
+        }
+        }, 1000)}
 
 function start(){
     sec = 1500;
@@ -18,11 +25,14 @@ function start(){
         sec = sec-1;
         min = Math.floor(sec/60);
         ele.innerHTML = min + ":" + sec%60
+        ele1.innerHTML = "work/study :)"
+
         if(sec <= 0){
             clearInterval(pomodoro);
             startBreak();
         }
         }, 1000)}
+        
 
 
 function reset(){
@@ -31,6 +41,7 @@ function reset(){
     sec = 1500;
     ele.innerHTML = "25" + ":" + "00"
     document.querySelector("body","header","h2","p").style.background = "#c795ec"
+    ele1.innerHTML = "click start to begin your next study session"
     return;
 }
 
@@ -42,7 +53,11 @@ function reset(){
     clearInterval(pomodoro);
     sec = 1500;
     ele.innerHTML = "25" + ":" + "00"
+    ele1.innerHTML = "click start to begin study session"
     document.querySelector("body","header","h2","p").style.background = "#c795ec"
     return;}
 
 )()
+
+
+
