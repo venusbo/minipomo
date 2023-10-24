b162209d1a2cef7c550232dff1db29deb803ea20
John <johnkim1@outlook.com>
2023-10-24 23:53:50 +1100

    referred to timerWorker in listening and posting functions as self

--- timer-worker.js
+++ timer-worker.js
@@ -1,4 +1,4 @@
-timerWorker.onmessage = (ev) => {
+self.onmessage = (ev) => {
 	console.log("message recieved from main script")
 	if (ev.data === "start") {
 		 sec = 1500;
@@ -6,13 +6,13 @@ timerWorker.onmessage = (ev) => {
 		setInterval(() => {
 		sec = sec-1;
         min = Math.floor(sec + sec/60);
-        postMessage(sec);
+        self.postMessage(sec);
 		console.log("Tick, tick, posting sec message back to main script")
 
         if(sec <= 0){
 			console.log("End pomodoro event triggered")
             clearInterval(pomodoro);
-            postMessage("startBreak");
+            self.postMessage("startBreak");
         }
         }, 1000)}
 }
