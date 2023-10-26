a2389279941855fa849e8e84960aa4027fd8764c
John <johnkim1@outlook.com>
2023-10-26 12:32:30 +1100

    adding timer-worker2 to mainbranch

---
+++ timer-worker2.js
@@ -0,0 +1,25 @@
+self.onmessage = (ev) => {
+	console.log("start message recieved from main script to timerworker2")
+	
+	if (ev.data === "reset") {
+		console.log("reset message recieved from mainscript to timerworker2")
+		sec = 0; // reset sec to 0
+		clearInterval(timer); // reset SetInterval
+		};
+
+    if (ev.data === "pause"){
+        console.log("pause message recieved from mainscript to timerworker2")
+    }
+	
+	if (ev.data === "start") {
+        sec = 0;
+        timer = setInterval(()=>{
+		sec = sec + 1;
+        self.postMessage(sec);
+		console.log("Tick, tick, posting sec message back to main script");
+		console.log(sec);
+            
+        }, 1000)
+        return;
+    }
+}
